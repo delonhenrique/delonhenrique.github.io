5979170eec86c8ab20900305bb95276edfb1cdb6
Delon Henrique <delon.henrique@gmail.com>
2022-06-07 10:56:03 -0300

    Home changes

---
+++ src/components/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = (props) => {
+    return (
+        <div>
+            <h1>Delon Henrique</h1>
+            <h2>Bem vindo à minha página de perfil!</h2>
+            <h4>Esta é uma página de perfil profissional desenvolvida em React</h4>
+            <p>No menu ao lado você encontrará alguns exemplos de códigos</p>
+        </div>
+    )
+}
+
+export default Home
