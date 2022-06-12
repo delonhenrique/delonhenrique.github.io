61e44a0c18d742de0c8d43adff1ebcc602c095a9
Delon Henrique <delon.henrique@gmail.com>
2022-06-12 19:43:53 -0300

    profile-picture

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
+import './Home.css'
+
+import profilePicture from '../../images/profile-pic.png'
+
 const Home = (props) => {
     return (
-        <div>
+        <div className="mainHome">
+            <div className="div-picture">
+                <img src={profilePicture} alt="" className="profile-picture" />
+            </div>
             <h1>Delon Henrique</h1>
-            <h2>Bem vindo à minha página de perfil!</h2>
-            <h4>Esta é uma página de perfil profissional desenvolvida em React</h4>
-            <p>No menu ao lado você encontrará alguns exemplos de códigos</p>
+            <h2>Desenvolvedor</h2>
+            <h4>Experência profissional</h4>
+            <h5>Docente e coordenador - Técnico em Mecatrônica</h5>
+            <p><strong>Período:</strong> 5 anos</p>
+            <p>Algumas das disciplinas lecionadas:
+                Lógica de programação
+                Eletrônica Digital
+                Eletropneumática
+                CLP (Controlador Lógico Programável)
+            </p>
         </div>
     )
 }
