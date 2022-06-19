c65734a26f05d626aaa6f520f5b65af3f53f23a4
Delon Henrique <delon.henrique@gmail.com>
2022-06-19 18:10:29 -0300

    Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,16 +7,9 @@ import Button from "./Button";
 const Projects = (props) => {
     return (
         <div className="card inLine">
-            <Button title="Netflix Catalog" link="#" />
-            <Button title="Netflix Catalog" link="#" />
-            <Button title="Netflix Catalog" link="#" />
-            <Button title="Netflix Catalog" link="#" />
-            <Button title="Netflix Catalog" link="#" />
-            <Button title="Netflix Catalog" link="#" />
-            <Button title="Netflix Catalog" link="#" />
-            <Button title="Netflix Catalog" link="#" />
             <Button title="Jump Game"
                 link="https://delonhenrique.github.io/jump-game/" />
+            <Button title="Hooks - React" link="https://delonhenrique.github.io/hooks-react/" />
         </div>
     )
 }
